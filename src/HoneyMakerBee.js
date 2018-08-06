545daaf7dbd9b057afbe09d855697a018dedaca8
MattRodigheri <mrodigheri@gmail.com>
2018-08-06 12:53:49 -0700

    passing tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, job, color) {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+// a makeHoney method that adds 1 to that honeyBee\'s honeyPot
+// a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
